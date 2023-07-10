39fc07278c1412a6b870dd8059d9d91f25e54624
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-07-10 15:17:54 +0200

    menubar

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+
+@NgModule({
+  exports: [
+    MenubarModule,
+    MenuModule,
+  ]
+})
+export class PrimeNgModule { }
