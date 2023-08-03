c0372cce334b6388b4ca9cccb4b5756e689696b3
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-08-03 09:14:08 +0200

    fieldset + pipes de i18Select y i18nPlural

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Pepe';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient():void {
+    this.name = 'Pepita';
+    this.gender = 'female';
+  }
+
+  // i18n Plural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': ' no tenemos ningún cliente esperando.',
+    '=1': ' tenemos un cliente esperando.',
+    '=2': ' tenemos 2 personas esperando.',
+    'other': ' tenemos # clientes esperando.',
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
+
 }
